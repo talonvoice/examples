9fb74f0bfb8892740bb3ca89300cd09dcf3b936f
Ryan Hileman <lunixbochs@gmail.com>
2018-03-05 17:54:03 -0800

    remove topic from autosleep

--- autosleep.py
+++ autosleep.py
@@ -1,6 +1,6 @@
 from talon.engine import engine
 
-def on_status(topic, j):
+def on_status(j):
     if j['cmd'] == 'mic' and j['status'] == 'on':
         print('mic turned on, mimic "go to sleep"')
         engine.mimic('go to sleep')
