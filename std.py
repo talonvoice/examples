289d3e1e85e71b46d1e06c403c7965e2c56189aa
Ryan Hileman <lunixbochs@gmail.com>
2018-09-10 09:59:46 -0700

    update the alphabet

--- std.py
+++ std.py
@@ -3,7 +3,7 @@ from talon import ctrl, clip
 from talon_init import TALON_HOME, TALON_PLUGINS, TALON_USER
 import string
 
-alpha_alt = 'air bat cap die each fail gone harm sit jury crash look mad near odd pit quest red sun trap urge vest whale box yes zip'.split()
+alpha_alt = 'air bat cap drum each fame gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip'.split()
 ###
 alnum = list(zip(alpha_alt, string.ascii_lowercase)) + [(str(i), str(i)) for i in range(0, 10)]
 
