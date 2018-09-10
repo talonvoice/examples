0fe223608fd3e66e3da780d6d0d00e7d8d9ef846
Ryan Hileman <lunixbochs@gmail.com>
2018-09-10 10:09:31 -0700

    fame -> fine

--- std.py
+++ std.py
@@ -3,7 +3,7 @@ from talon import ctrl, clip
 from talon_init import TALON_HOME, TALON_PLUGINS, TALON_USER
 import string
 
-alpha_alt = 'air bat cap drum each fame gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip'.split()
+alpha_alt = 'air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip'.split()
 ###
 alnum = list(zip(alpha_alt, string.ascii_lowercase)) + [(str(i), str(i)) for i in range(0, 10)]
 
