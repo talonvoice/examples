b02d7a646e1ef66f24b6b38896679c1475c2f265
Ryan Hileman <lunixbochs@gmail.com>
2019-06-28 10:50:55 -0700

    add 'zoom mouse' to eye control commands

--- eye_control.py
+++ eye_control.py
@@ -1,8 +1,9 @@
-from talon_plugins import eye_mouse
+from talon_plugins import eye_mouse, eye_zoom_mouse
 from talon.voice import Context
 
 ctx = Context('eye_control')
 ctx.keymap({
+    'zoom mouse':      lambda m: eye_zoom_mouse.active.toggle(),
     'debug overlay':   lambda m: eye_mouse.debug_overlay.toggle(),
     'control mouse':   lambda m: eye_mouse.control_mouse.toggle(),
     'camera overlay':  lambda m: eye_mouse.camera_overlay.toggle(),
